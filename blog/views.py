880a062d378df3bb0f03b25acd297f3a80d10148
Непряхин Алексей <aleksey.nepryahin@outlook.com>
2023-03-30 17:58:24 +0300

    Show five most popular posts on main page

--- blog/views.py
+++ blog/views.py
@@ -26,10 +26,14 @@ def serialize_tag(tag):
         'posts_with_tag': len(Post.objects.filter(tags=tag)),
     }
 
+def get_likes_count(post):
+    return post.likes.count()
 
 def index(request):
 
-    most_popular_posts = []  # TODO. Как это посчитать?
+    posts = Post.objects.all()
+    sorted_posts = sorted(posts, key=get_likes_count, reverse=True)
+    most_popular_posts = list(sorted_posts)[:5]
 
     fresh_posts = Post.objects.order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
