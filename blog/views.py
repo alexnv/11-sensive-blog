aa82a9cb267df983f2eaf64147417070ea95c5f8
Непряхин Алексей <aleksey.nepryahin@outlook.com>
2023-03-31 17:59:27 +0300

    cleanup the code.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.db.models import Count, Prefetch
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from blog.models import Post, Tag
 
@@ -47,7 +47,8 @@ def index(request):
 
 
 def post_detail(request, slug):
-    post = Post.objects.select_related('author').annotate(likes_count=Count('likes')).get(slug=slug)
+    #post = Post.objects.select_related('author').annotate(likes_count=Count('likes')).get(slug=slug)
+    post = get_object_or_404(Post.objects.select_related('author').annotate(likes_count=Count('likes')), slug=slug)
     comments = post.comments.select_related('author')
     serialized_comments = []
     for comment in comments:
